f49b699b0544c7e0ee4efd8377de1949938bf6a5
7ijme <tijmevh@gmail.com>
2023-02-03 15:56:03 +0100

    data > assets/data

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -4,8 +4,8 @@ import "../Styles/container.css";
 import About from "./About";
 import Skills, { SkillType } from "./Skills";
 import Projects, { ProjectType } from "./Projects";
-import Contact from "./Contact";
 import Footer from "./Footer";
+import data from "../assets/data.json";
 
 type Props = {};
 
@@ -15,25 +15,6 @@ export type Data = {
 };
 
 export default function Container({}: Props) {
-  const [data, setData] = React.useState<Data | null>(null);
-
-  const fetchJson = () => {
-    fetch("./data.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((e: Error) => {
-        console.log(e.message);
-      });
-  };
-
-  React.useEffect(() => {
-    fetchJson();
-  }, []);
-
   return (
     <div className="container">
       <Header />
