34b1b44f4b066304bcd1201144b360d91d6d36ff
7ijme <tijmevh@gmail.com>
2023-02-09 20:44:48 +0100

    bah

--- src/Components/Rate.tsx
+++ src/Components/Rate.tsx
@@ -10,7 +10,7 @@ export default function Rate({}: Props) {
 
     if (rate == null) return;
 
-    fetch("http://tijmevh.nl:3000/rate", {
+    fetch("https://tijmevh.nl:3000/rate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
