89ab77ce1885edbfd9b000e73ea7bebe20bf27b2
7ijme <tijmevh@gmail.com>
2023-02-12 19:26:18 +0100

    no brrr

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -12,21 +12,25 @@ export default function About({}: Props) {
         Hello there! My name is Tijme and I am a developer. I have been
         passionate about programming since I was eleven years old, and have
         since dedicated my life to mastering the art of coding.
-        <br /> <br />
+      </p>
+      <p>
         Aside from programming, I have a love for fitness and sports. I
         regularly hit the gym to maintain a healthy and active lifestyle, and
         also enjoy playing korfbal in my free time.
-        <br /> <br />
+      </p>
+      <p>
         Although I may be young, I am eager to gain as much experience as
         possible and continue to grow in my career. I am a quick learner and am
         always eager to take on new challenges. I believe that with dedication
         and hard work, I can make my mark in the tech industry and become a
         valuable contributor to any team.
-        <br /> <br />
+      </p>
+      <p>
         I am also open to learning from experienced developers and am eager to
         collaborate on projects with others who share my passion for
         programming.
-        <br /> <br />
+      </p>
+      <p>
         I believe that working in teams is essential for success and am
         committed to contributing to a positive and productive work environment.
         I have excellent communication skills and am comfortable working with
@@ -34,7 +38,8 @@ export default function About({}: Props) {
         team captain of my sports team, which has helped me develop strong
         interpersonal skills. I am confident that I can bring these skills to
         any team I work with and contribute to the success of the project.
-        <br /> <br />
+      </p>
+      <p>
         So if you're looking for a young and motivated developer, look no
         further. I am ready to take on new challenges and gain valuable
         experience while making a positive impact in the tech world.
