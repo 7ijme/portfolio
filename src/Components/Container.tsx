518f5d49337b5d08c3ab99a68824b0f2013c57e8
7ijme <tijmevh@gmail.com>
2023-02-27 15:23:21 +0100

    better

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -1,13 +1,13 @@
 import Header from "./Header";
-import "../Styles/container.scss";
 import About from "./About";
 import Skills, { SkillType } from "./Skills";
 import Projects, { ProjectType } from "./Projects";
 import Footer from "./Footer";
-import data from "../assets/data.json";
 import Contact from "./Contact";
 import Rate from "./Rate";
 import Introduction from "./Introduction";
+import "../Styles/container.scss";
+import data from "../assets/data.json";
 
 type Props = {};
 
